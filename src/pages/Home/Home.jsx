46cf57bd5dfa4880a1cc5429c202bd2161ef21e7
Shorifuzzaman2020 <shorifuzzamansoil2020@gmail.com>
2025-10-23 23:12:17 +0600

    banner section done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            this is home page
+        <div className='w-11/12 mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 my-8'>
+            <div className=''>
+                <img className='rounded-lg' src="https://i.ibb.co.com/XrD0DQHZ/428259959-1049583919456470-5932274881690033579-n.jpg" alt="" />
+            </div>
+            <div className="h-74">
+                <img
+                    src="https://i.ibb.co.com/DDbv4q69/484176661-1050460523789100-2658054001492228975-n.jpg"
+                    alt=""
+                    className="h-full w-full object-cover rounded-lg"
+                />
+            </div>
+
         </div>
     );
 };
